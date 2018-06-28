577a0df8bcc4de545dad325de24697985ee491fa
bnsnchz <bsanchez4488@gmail.com>
2018-06-28 13:12:04 -0700

    pushing user model and server

---
+++ models/user.js
@@ -0,0 +1,31 @@
+module.exports=(sequelize, DataTypes)=>{
+    var User = sequlaize.define("User", {
+        screen_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                len:[1]
+            }
+        },
+        image: {
+            type: DataTypes.STRING,
+            defaultValue: "/assets/default.png"
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+        
+    });
+
+    User.associate = (models)=>{
+        User.belongsToMany(models.Game, {
+            through: "user2game"
+        });
+        User.hasMany(models.Post, {
+            onDelete: "cascade"
+        })
+    }
+
+    return User;
+};
