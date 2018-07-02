9ae251f1913249e4eceb16a2427ae9df9b746248
bnsnchz <bsanchez4488@gmail.com>
2018-07-02 10:47:00 -0700

    updating

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,148 +1,3 @@
-<<<<<<< HEAD
-const db = require('../models/index.js');
-const encrypt = require('../encryption.js');
-const igdb = require('igdb-api-node').default;
-const client = igdb('1ef55fd89628c4844a334b3bee9b4194');
-
-var user = require("../models/user.js")
-
-
-module.exports = function (app) {
-    //need feeback from Alex on how to use api searches to display games on front page/home page
-    app.get('/', (req, res) => {
-        client.games({
-                fields: 'name', // Return all fields
-                limit: 5, // Limit to 5 results
-                offset: 15, // Index offset for results
-                search: "halo"
-            }, [
-                "name",
-                "release_dates.date",
-                "rating",
-                "cover"
-            ])
-            .then((response) => {
-                // response.body contains the parsed JSON response to this query
-                res.render("index", response);
-                //res.render('index', response);
-            }).catch(error => {
-                throw error;
-            });
-    });
-
-    //*****need feedback from Alex on how to do api searches on single game titles****
-    app.get('/game/:game', (req, res) => {
-        client.games({
-                fields: 'name', // Return all fields
-                limit: 5, // Limit to 5 results
-                offset: 15, // Index offset for results
-                search: "halo"
-            }, [
-                "name",
-                "release_dates.date",
-                "rating",
-                "cover"
-            ])
-            .then((response) => {
-                // response.body contains the parsed JSON response to this query
-                res.render("index", response);
-                //res.render('index', response);
-            }).catch(error => {
-                throw error;
-            });
-    });
-
-
-    app.get("/user/:screen_name", (req, res) => {
-        db.User.findOne({
-            where: {
-                screen_name: req.params.screen_name
-            }
-        }).then((data) => {
-            var userInfo = data.dataValues;
-            //    console.log(data);
-            console.log(data.dataValues);
-            //    res.json(response);
-            res.render("profile", userInfo);
-        })
-
-    });
-
-    app.get("/user/:screen_name", (req, res) => {
-        db.user2game.findOne({
-            where: {
-                screen_name: req.params.screen_name
-            }
-        }).then((data) => {
-            var userInfo = data.dataValues;
-            //    console.log(data);
-            console.log(data.dataValues);
-            //    res.json(response);
-            res.render("profile", userInfo);
-        })
-
-    });
-
-    app.get('/createreview', (req, res) => {
-        res.render('review', res);
-    });
-
-    app.post('/review/:id', (req, res) => {
-        db.Post.create({
-            body: req.body.body,
-            Userid: db.User.id
-        }).then((result) => {
-            res.send(result);
-        });
-    });
-
-    app.get('/review/:id', (req, res) => {
-        db.Post.findOne({
-            where: {
-                id: req.params.id
-            }
-        }).then((result) => {
-            res.render('', result); //we need a view review handlebars
-        })
-    });
-
-
-    app.get("/login", (req, res) => {
-        res.render("signin");
-    });
-
-    app.get('/register', (req, res) => {
-        res.render('register');
-    })
-
-
-
-    app.post('/signin', (req, res) => {
-        console.log(req.session)
-
-        db.User.findOne({
-            where: {
-                screen_name: req.body.screen_name,
-            }
-        }).then((results) => {
-            if (results.screen_name === req.body.screen_name && encrypt.decrypt(results.password) === req.body.password) {
-                var token = 't' + Math.random();
-                results.token = token;
-                res.cookie('token', token);
-                req.session.user = results;
-                db.User.update({
-                    token: token,
-                    where: {
-                        screen_name: req.body.screen_name
-                    },
-                }).then((response) => {
-                    return res.render('profile', response)
-                });
-
-            } else {
-                return res.send('Sorry, account was not found.')
-            }
-=======
 const db = require("../models/index.js");
 const encrypt = require("../encryption.js");
 const igdb = require("igdb-api-node").default;
@@ -281,7 +136,6 @@ module.exports = function(app) {
           screen_name: req.body.screen_name,
           password: encrypt.encrypt(req.body.password),
           routeName: req.body.screen_name.replace(/\s+/g, "").toLowerCase()
->>>>>>> userapi1
         })
           .then(response => {
             res.json(response);
@@ -304,11 +158,11 @@ module.exports = function(app) {
           });
       }
     });
-<<<<<<< HEAD
+  });
 
     app.get('/register', (req, res) => {
         res.render('register');
-    })
+    });
 
     //////////check if user is logged into current session when attempting to submit reviews//////////
 
@@ -350,10 +204,4 @@ module.exports = function(app) {
         });
     });
 
-
-
 }
-=======
-  });
-};
->>>>>>> userapi1
